936c1671d03fd05f4d70fa57b73114022ca6a098
Dylan Holmes <holmes.dylan@hotmail.com>
2021-11-28 20:55:01 -0800

    - Updated version #

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ctdb_utility_lib",
     packages=find_packages(include=["ctdb_utility_lib"]),
-    version="0.1.0",
+    version="0.1.1",
     description="Contact Tracer DB Utility Python library",
     install_requires=["pytz"],
     author="Ahmed",
