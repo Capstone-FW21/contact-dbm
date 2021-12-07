75cbb70da4d72161db7530b5624ecd2f7877ca77
dulaimi99 <ahmeddulaimi99@gmail.com>
2021-12-07 21:25:20 +0000

    setup.py updates

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import find_packages, setup
 setup(
     name='ctdb_utility_lib',
     packages=find_packages(include=['ctdb_utility_lib']),
-    version='0.1.0',
-    description='Contact Tracer DB Utility Python library',
-    install_requires = ['pytz'],
+    version='0.1.2',
+    description='Contact Tracer Database Utility Python Library',
     author='Me',
     license='MIT',
 )
