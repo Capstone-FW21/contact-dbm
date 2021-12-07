4d23bf46955773add0c172f9063aa724e54b4856
Ahmed <ahmeddulaimi99@gmail.com>
2021-12-07 14:08:18 -0800

    readd setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ctdb_utility_lib",
+    packages=find_packages(include=["ctdb_utility_lib"]),
+    version="0.1.3",
+    description="Contact Tracer DB Utility Python library",
+    author="Ahmed",
+    license="MIT",
+)
