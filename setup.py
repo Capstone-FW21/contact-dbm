1095ee29af4c4b2566924fab23f53cf237ad5cc1
Ahmed <ahmeddulaimi99@gmail.com>
2021-12-08 10:04:28 -0800

    adding setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ctdb_utility_lib",
     packages=find_packages(include=["ctdb_utility_lib"]),
-    version="0.1.3",
+    version="0.1.4",
     description="Contact Tracer DB Utility Python library",
     author="Ahmed",
     license="MIT",
