95b7bbfd1097b1942fffdb5fa5af59ce75c8dd00
Dylan Holmes <holmes.dylan@hotmail.com>
2022-01-03 17:39:55 -0800

    - Removed redundant connections

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ctdb_utility_lib",
     packages=find_packages(include=["ctdb_utility_lib"]),
-    version="0.1.5",
+    version="0.1.7",
     description="Contact Tracer DB Utility Python library",
     install_requires=["psycopg2"],
     author="Ahmed",
