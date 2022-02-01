5b99727fbd7805efe22500b071aed1938107e370
Ahmed <ahmeddulaimi99@gmail.com>
2022-02-01 15:54:46 -0800

    incremented version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ctdb_utility_lib",
     packages=find_packages(include=["ctdb_utility_lib"]),
-    version="0.3.0",
+    version="0.3.1",
     description="Contact Tracer DB Utility Python library",
     install_requires=["psycopg2"],
     author="[Ahmed, Dylan]",
