a7aff4c2c9d537d02e269e44716cdd7664251adb
Dylan Holmes <holmes.dylan@hotmail.com>
2022-02-03 10:07:44 -0800

    - Added personal scan entry

--- ctdb_utility_lib/utility.py
+++ ctdb_utility_lib/utility.py
@@ -15,17 +15,22 @@ def connect_to_db():
         host="34.134.212.102",
     )
 
+
 # check email format
 def valid_email_format(email: str) -> bool:
     regex = "^[A-Za-z0-9]+[\._]?[A-Za-z0-9]+[@]\w+[.]\w{2,3}$"
 
     return re.search(regex, email) != None
 
-def valid_aspect_ratio_format(aspect_ratio:str) -> bool:
-    
+
+def valid_aspect_ratio_format(aspect_ratio: str) -> bool:
+
     width_length = aspect_ratio.split(":")
-    
-    return len(width_length) == 2 and width_length[0].isnumeric() and width_length[1].isnumeric()
+
+    return (
+        len(width_length) == 2 and width_length[0].isnumeric() and width_length[1].isnumeric()
+    )
+
 
 def _execute_statement(conn, statement):
     """
@@ -40,12 +45,31 @@ def _execute_statement(conn, statement):
     return cursor
 
 
-def get_room_aspect_ratio(room_id:str,conn) -> list:
-    
+def get_room_aspect_ratio(room_id: str, conn) -> list:
+
     # run query to get aspect ratio for specified room
-    cur = _execute_statement(conn,f"SELECT aspect_ratio FROM rooms WHERE room_id = '{room_id}'")
+    cur = _execute_statement(
+        conn, f"SELECT aspect_ratio FROM rooms WHERE room_id = '{room_id}'"
+    )
     result = cur.fetchone()
     return result[0].split(":")
+
+
+def add_personal_scan(scanner_email: str, scanned_email: str, conn):
+    """
+    Adds a personal scan entry to the personal_scan table.
+    """
+    if not (valid_email_format(scanner_email) and valid_email_format(scanned_email)):
+        return -1
+
+    cur_date = datetime.now()
+
+    cur = _execute_statement(
+        conn,
+        f"INSERT INTO personal_scans (id, scanner_email, scanned_email, scan_time) VALUES (DEFAULT, '{scanner_email}', '{scanned_email}', TIMESTAMP '{cur_date}');",
+    )
+
+    return 0
 
 
 # add a scan to the scans table in db
@@ -56,11 +80,11 @@ def add_scan(email: str, room_id: str, x_pos: int, y_pos: int, conn):
     # Invalid email format
     if not valid_email_format(email):
         return -1
-    
-    width_length = get_room_aspect_ratio(room_id,conn)
-    
-    #invalid position
-    if(x_pos < 0 or x_pos > int(width_length[0]) or y_pos < 0 or y_pos > int(width_length[1])):
+
+    width_length = get_room_aspect_ratio(room_id, conn)
+
+    # invalid position
+    if x_pos < 0 or x_pos > int(width_length[0]) or y_pos < 0 or y_pos > int(width_length[1]):
         return -1
 
     # create current datetime obj
@@ -135,6 +159,7 @@ def get_person(email: str, conn):
 
     return result[0]
 
+
 # Checks if room with room_id already exists
 def exists_in_rooms(room_id: str, conn):
 
@@ -148,16 +173,17 @@ def exists_in_rooms(room_id: str, conn):
     else:
         return False
 
+
 # add room entry to room table
-def add_room(room_id: str, capacity: int, building_name: str, aspect_ratio: str ,conn):
-    
+def add_room(room_id: str, capacity: int, building_name: str, aspect_ratio: str, conn):
+
     if not (room_id and building_name and aspect_ratio):
         return -1
 
     # person exists in the people table
     if exists_in_rooms(room_id, conn):
         return -1
-    
+
     if not valid_aspect_ratio_format(aspect_ratio):
         return -1
 
@@ -169,6 +195,7 @@ def add_room(room_id: str, capacity: int, building_name: str, aspect_ratio: str 
     )
     return 0
 
+
 # retrieves room info
 # returns -1 if no match found
 def get_room(room_id: str, conn):
@@ -178,6 +205,7 @@ def get_room(room_id: str, conn):
     result = cur.fetchone()
 
     return result
+
 
 # retrieves all users from people table
 def get_all_users(conn):
